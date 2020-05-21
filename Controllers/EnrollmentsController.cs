126d4c4252b67272d292085622b94f04d1e85afc
s18985 <s18985@pjwstk.edu.pl>
2020-05-21 23:58:20 +0200

    zad10

---
+++ Controllers/EnrollmentsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw10.DAL;
+using cw10.DTOs.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw10.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+
+    public class EnrollmentsController : ControllerBase
+    {
+        private IStudentsDbService _service;
+
+        public EnrollmentsController(IStudentsDbService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+
+            
+                var response = _service.EnrollStudent(request);
+
+                if (response != null)
+                {
+                    return Created("", response);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            
+
+
+        }
+
+
+        [HttpPost("/api/enrollments/promotions")]
+        public IActionResult PromoteStudents(PromoteStudentsRequest request)
+        {
+            
+                var response = _service.PromoteStudents(request);
+
+                if (response != null)
+                {
+                    return Created("", response);
+                }
+                else
+                {
+                    return NotFound();
+                }
+          
+        }
+
+    }
+}
