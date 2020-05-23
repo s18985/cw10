d6eab836c30d16c588d88ebb738b7b685e40db2f
s18985 <s18985@pjwstk.edu.pl>
2020-05-23 20:21:46 +0200

    zad10 configuration

--- Startup.cs
+++ Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using cw10.DAL;
+using cw10.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,8 @@ namespace cw10
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<s18985Context>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
             services.AddScoped<IStudentsDbService, EFServerDbService>();
             services.AddControllers();
         }
